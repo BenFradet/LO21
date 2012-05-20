9dd46b5a64c0bffa03d3599c0612ae9af2ce2f81
BenjaminFradet <benjamin.fradet@gmail.com>
2012-05-20 15:29:05 +0200

    include.h

---
+++ include.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include "constante.h"
+#include "complexe.h"
+#include "entier.h"
+#include "expression.h"
+#include "pile.h"
+#include "rationnel.h"
+#include "reel.h"
+#include <cmath>
+
+#endif // INCLUDE_H
